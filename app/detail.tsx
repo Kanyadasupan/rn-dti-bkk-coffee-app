411a7905528bdc4193f7ef46ea26f7d175aeda3d
KanyadaSupan <s6652410028@sau.ac.th>
2026-02-04 16:00:33 +0700

    Second commit

---
+++ app/detail.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Detail() {
+  return (
+    <View>
+      <Text>D</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
